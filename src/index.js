88b8a776c93126f7b3a99390ed84454176490d05
Carlos Miguel <miguel21.melo21@gmail.com>
2018-11-06 19:05:55 -0300

    Mongoose configuration

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb://backend-twitter-realtime:goweek123@ds155213.mlab.com:55213/backend-twitter-realtime', {
+  useNewUrlParser: true,
+});
 
 app.get('/', (req, res) => {
   return res.send('Backend Twitter Realtime');
