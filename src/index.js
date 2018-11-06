51badcfd3239428be31f4b5b1b2f3832d6c698ec
Carlos Miguel <miguel21.melo21@gmail.com>
2018-11-06 19:52:57 -0300

    Socket.io implemented

--- src/index.js
+++ src/index.js
@@ -3,8 +3,17 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
 mongoose.connect('mongodb://backend-twitter-realtime:goweek123@ds155213.mlab.com:55213/backend-twitter-realtime', {
   useNewUrlParser: true,
+});
+
+app.use((req, use, next) => {
+  req.io = io;
+
+  return next();
 });
 
 app.use(express.json());
