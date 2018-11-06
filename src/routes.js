90060aeb68a6dc9b647d860e2569336f68387383
Carlos Miguel <miguel21.melo21@gmail.com>
2018-11-06 19:29:36 -0300

    Routes updated

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ const routes = express.Router();
 
 const TweetController = require('./controllers/TweetController');
 
-routes.get('tweets', TweetController.index);
-routes.post('tweets', TweetController.store );
+routes.get('/tweets', TweetController.index);
+routes.post('/tweets', TweetController.store );
 
 module.exports = routes;
